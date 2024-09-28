c8c793df922ab1e90ece289ef6029fd8055d3045
umatskoy <haqifarel@gmail.com>
2024-09-28 22:01:44 +0700

    Add files via upload

---
+++ Google Sheet.js
@@ -0,0 +1,11 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycby6DsqosJb302N0sEqoW-QvTBQGo52SuzuEe9PSSNJR0Z6-8AccyvximnUIOijoqVka4w/exec'
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("Thank you! your form is submitted successfully." ))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
+})
